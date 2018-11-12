5d6f0c13a1d97fa2fa95043cab885df9466fe381
Douglas Purdy <dpurdy@my.wgu.edu>
2018-11-12 06:51:17 -0500

    initial commit sandbox app

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { User } from '../models/User';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+
+export class UserComponent {
+  user: User;
+
+  constructor(){
+    this.user = {
+      firstName : 'John',
+      lastName : 'Doe',
+      age: 30,
+      address: {
+        street: '50 Main St.',
+        city: 'Boston',
+        state: 'MA'
+      }
+
+
+
+    }
+  }
+}
